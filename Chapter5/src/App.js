75a24885a87059ca97ac6368b3d26d9f60bfe2fe
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-02-15 16:39:52 +0200

    Add files via upload

---
+++ Chapter5/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  render() {        
+    return (
+      <div>
+        <UserForm />        
+                
+      </div>
+    );
+  }
+}
+
+export default App;
+
